0db0c3274a585b25076e2c0de5ed2e2d76bea59e
XHURCH <info@xhurch.net>
2024-01-29 14:40:40 -0800

    Brought in Hairless Monkey model and added animations + added cutscene cameras
    ditto

--- Unity-II/Assets/Scripts/CameraSwitcher.cs
+++ Unity-II/Assets/Scripts/CameraSwitcher.cs
@@ -4,30 +4,39 @@ using UnityEngine;
 
 public class CameraSwitcher : MonoBehaviour
 {
-    public GameObject cam1;
-    public GameObject cam2;
-    public GameObject cam3;
+    //Camera Switcher for any number of cameras, be sure to adjust number of cameras in Update.
+    public GameObject[] cam;
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Alpha1))
         {
-            cam1.SetActive(true);
-            cam2.SetActive(false);
-            cam3.SetActive(false);    
+            cam[0].SetActive(true);
+            cam[1].SetActive(false);
+            cam[2].SetActive(false);
+            cam[3].SetActive(false); 
         }
         if(Input.GetKeyDown(KeyCode.Alpha2))
         {
-            cam1.SetActive(false);
-            cam2.SetActive(true);
-            cam3.SetActive(false);    
+            cam[0].SetActive(false);
+            cam[1].SetActive(true);
+            cam[2].SetActive(false);
+            cam[3].SetActive(false);     
         }
         if(Input.GetKeyDown(KeyCode.Alpha3))
         {
-            cam1.SetActive(false);
-            cam2.SetActive(false);
-            cam3.SetActive(true);    
+            cam[0].SetActive(false);
+            cam[1].SetActive(false);
+            cam[2].SetActive(true);
+            cam[3].SetActive(false);    
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            cam[0].SetActive(false);
+            cam[1].SetActive(false);
+            cam[2].SetActive(false);
+            cam[3].SetActive(true);    
         }
     }
 }
---
+++ Unity-II/Assets/Scripts/CameraSwitcherMiri.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitcherMiri : MonoBehaviour
+{
+    // int camSelect = 0;
+
+    // void Update() 
+    // {
+    // if(Input.GetKeyDown(KeyCode.Alpha1)
+    // {
+    //     camSelect = 0;
+    // } 
+    // else if(Input.GetKeyDown(KeyCode.Alpha2)
+    // {
+    // camSelect = 1;
+    // }
+    // if (Input.anyKey) 
+    // {
+    //     for (int i = 0; i < sizeof(cam); i++) {
+    //         if (cam[i] = camSelect) 
+    //         {
+    //             cam[i].SetActive(true);
+    //         }
+    //     }
+    // }
+    // else 
+    // {
+    //     cam[i].SetActive(false);
+    // }
+    // }
+}
