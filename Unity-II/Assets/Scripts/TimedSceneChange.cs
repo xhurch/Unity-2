a139d86dfadd82acfdf6b950dfbb982a2b6e5f63
XHURCH <info@xhurch.net>
2024-02-07 14:54:17 -0800

    Added Unity Chan Model, fixed textures and materials, added animation states

--- Unity-II/Assets/Scripts/TimedSceneChange.cs
+++ Unity-II/Assets/Scripts/TimedSceneChange.cs
@@ -2,26 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.VFX;
 
 public class TimedSceneChange : MonoBehaviour
 {
     public float sceneChangeDelay;
-    public VisualEffect portal;
 
     public int sceneToLoad = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(SceneChangeWithDelay());
+        Invoke("SceneChange", sceneChangeDelay);
     }
 
-    IEnumerator SceneChangeWithDelay()
+    void SceneChange()
     {
-        yield return new WaitForSeconds(2);
-        portal.enabled = true;
-        yield return new WaitForSeconds(sceneChangeDelay);
         SceneManager.LoadScene(sceneToLoad);
     }
 }
