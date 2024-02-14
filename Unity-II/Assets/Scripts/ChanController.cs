05ecd11a042356d3f0301243956dfb9d11d9d09c
XHURCH <info@xhurch.net>
2024-02-14 14:48:40 -0800

    Update Animator Controller (Scratch), Start new scene "StonedApe_Level1

--- Unity-II/Assets/Scripts/ChanController.cs
+++ Unity-II/Assets/Scripts/ChanController.cs
@@ -2,54 +2,61 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChanController : MonoBehaviour
-{
+public class ChanController : MonoBehaviour{
     Animator chanim;
     Rigidbody rb;
-
     float inputH;
     float inputV;
-
     bool run;
-
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
         chanim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
+        run = false;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.Alpha1))
-        {
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.Alpha1)){
             chanim.Play("WAIT01");
         }
-        if(Input.GetKeyDown(KeyCode.Alpha2))
-        {
+
+        if(Input.GetKeyDown(KeyCode.Alpha2)){
             chanim.Play("WAIT02");
         }
-        if(Input.GetKeyDown(KeyCode.Alpha3))
-        {
+        
+        if(Input.GetKeyDown(KeyCode.Alpha3)){
             chanim.Play("WAIT03");
         }
-        if(Input.GetKeyDown(KeyCode.Alpha4))
-        {
-            chanim.Play("WAIT04", -1, 0.15f);
+
+        if(Input.GetKeyDown(KeyCode.Alpha4)){
+            chanim.Play("WAIT04");
         }
-        if(Input.GetMouseButtonDown(0))
-        {
+
+        if(Input.GetMouseButtonDown(0)){
             int n = Random.Range(0, 2);
 
-            if(n == 0)
-            {
+            if(n == 0){
                 chanim.Play("DAMAGED00");
             }
-            else
-            {
+            
+            if(n == 1){
                 chanim.Play("DAMAGED01");
             }
+        }
+        if(Input.GetKey(KeyCode.LeftShift)){
+            run = true;
+        }
+        else{
+            run = false;
+        }
+
+        if(Input.GetKey(KeyCode.Space)){
+            chanim.SetBool("jump", true);
+        }
+        else
+        {
+            chanim.SetBool("jump", false);
         }
 
         inputH = Input.GetAxis("Horizontal");
@@ -57,11 +64,19 @@ public class ChanController : MonoBehaviour
 
         chanim.SetFloat("inputH", inputH);
         chanim.SetFloat("inputV", inputV);
+        chanim.SetBool("run", run);
 
         float moveX = inputH * 20f * Time.deltaTime;
         float moveZ = inputV * 50f * Time.deltaTime;
 
-        rb.velocity = new Vector3(moveX, 0, moveZ);
+        if(moveZ <= 0){
+            moveX = 0f;
+        }
+        else if(run == true){
+            moveX *= 3f;
+            moveZ *= 3f;
+        }
 
+        rb.velocity = new Vector3(moveX, 0, moveZ);
     }
 }
